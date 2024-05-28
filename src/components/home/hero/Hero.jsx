60f33e5641d3d115fc71b143ce8ceaf5ed0dd1aa
AnikHaque <ekramul15-811@diu.edu.bd>
2024-05-28 23:48:00 +0600

    tested

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -11,39 +11,6 @@ const Hero = () => {
             title="Search Your Next Home"
             subtitle="Find new & featured property located in your local city."
           />
-
-          <form className="flex space-x-4 items-center rounded-lg shadow-lg bg-white p-4">
-            <div className="box flex items-center">
-              <span className="mr-2">City/Street</span>
-              <input
-                type="text"
-                placeholder="Location"
-                className="border-b-2 focus:outline-none focus:border-blue-500"
-              />
-            </div>
-            <div className="box flex items-center">
-              <span className="mr-2">Property Type</span>
-              <input
-                type="text"
-                placeholder="Property Type"
-                className="border-b-2 focus:outline-none focus:border-blue-500"
-              />
-            </div>
-            <div className="box flex items-center">
-              <span className="mr-2">Price Range</span>
-              <input
-                type="text"
-                placeholder="Price Range"
-                className="border-b-2 focus:outline-none focus:border-blue-500"
-              />
-            </div>
-            <div className="box">
-              <h4>Advance Filter</h4>
-            </div>
-            <button className="btn1">
-              <i className="fa fa-search"></i>
-            </button>
-          </form>
         </div>
       </section>
     </>
