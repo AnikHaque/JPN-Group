62346dbfa9de6488dedadda7a8dcf4fa8321529b
AnikHaque <ekramul15-811@diu.edu.bd>
2024-05-30 20:12:56 +0600

    updated about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,6 @@ import Back from "../common/Back";
 import Heading from "../common/Heading";
 import img from "../images/about.jpg";
 import "./about.css";
-import Basic from "../basic/Basic";
 
 const About = () => {
   return (
@@ -13,23 +12,29 @@ const About = () => {
         <div className="container flex mtop">
           <div className="left row">
             <Heading
-              title="Our Agency Story"
-              subtitle="Check out our company story and work process"
+              title="Message from us"
+              subtitle="Check out our company story says"
             />
 
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              PN MODERN LUCKY CITY is a new horizon of lifestyle. Its objectives
+              to create the new landscape of the city and upgrade the daily life
+              of city dwellers. Multiple families create a community. That is
+              why we introduce various types of community space to meet others.
+              These spaces like children playground, Multipurpose convention
+              hall, amphitheater, etc.
             </p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip.
+              People demand safety and security to their family members are sage
+              in our highly integrated designed city. All the safety measures
+              count here. This shall be the habitat for those who want to roam
+              in nature with fresh air int an exchangeable cultural environment.
+              That we designed. In one sentence- This JPN MODERN LUCKY CITY we
+              called towards new urban experience what we assure to your family.
             </p>
-            <button className="btn2">More About Us</button>
+            <button className="bg-[#27ae60] text-white p-4">
+              More About Us
+            </button>
           </div>
           <div className="right row">
             <img src="./immio.jpg" alt="" />
--- src/components/home/featured/FeaturedCard.jsx
+++ src/components/home/featured/FeaturedCard.jsx
@@ -5,12 +5,21 @@ import "./Featured.css";
 const FeaturedCard = () => {
   return (
     <>
-      <div className="content grid grid-cols-5 gap-4 mt-4">
+      <div className="content grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-4">
         {featured.map((items, index) => (
-          <div className="box hover-rotate" key={index}>
-            <img src={items.cover} alt={items.name} className="rounded-full" />
-            <h4 className="mt-2">{items.name}</h4>
-            <label className="text-gray-500">{items.total}</label>
+          <div
+            className="box hover-rotate p-4 bg-white rounded-lg shadow-md"
+            key={index}
+          >
+            <img
+              src={items.cover}
+              alt={items.name}
+              className="w-full h-48 object-cover rounded-full mx-auto"
+            />
+            <h4 className="mt-2 text-center">{items.name}</h4>
+            <label className="text-gray-500 text-center block">
+              {items.total}
+            </label>
           </div>
         ))}
       </div>
